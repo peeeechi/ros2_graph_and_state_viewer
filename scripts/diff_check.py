fc12662ee94504e5a35497934a6b4ec1acbd4f2b
satoshi.inoue <satoshi.inoue@tier4.jp>
2025-12-09 11:53:58 +0900

    feat: change 'param' structure in diff_check.py from list to dictionary for improved parameter tracking

--- scripts/diff_check.py
+++ scripts/diff_check.py
@@ -19,21 +19,21 @@ diff_map = {
         'topics': [],
         "nodes": [],
         "services": [],
-        'param': [],
+        'param': {},
         'connections': [],
     },
     'add': {
         'topics': [],
         "nodes": [],
         "services": [],
-        'param': [],
+        'param': {},
         'connections': [],
     },
     'change': {
         'topics': [],
         "nodes": [],
         "services": [],
-        'param': [],
+        'param': {},
         'connections': [],
     },
 }
@@ -53,8 +53,14 @@ for key in keys:
         before_names = before.keys()
         after_names = after.keys()
     else:
-        before_names = [f"{item['source_id']} -> {item['target_id']}({item['type']}:{item['direction']})"  for item in before]
-        after_names = [f"{item['source_id']} -> {item['target_id']}({item['type']}:{item['direction']})"  for item in after]
+        before_names = [
+            f"{item['source_id']} -> {item['target_id']}({item['type']}:{item['direction']})"
+            for item in before
+        ]
+        after_names = [
+            f"{item['source_id']} -> {item['target_id']}({item['type']}:{item['direction']})"
+            for item in after
+        ]
 
     for idx_before, b_n in enumerate(before_names):
 
@@ -63,7 +69,10 @@ for key in keys:
             if key == 'nodes':
                 params = before[b_n]['parameters']
                 for p in params:
-                    diff_map['missing']['param'].append(f"[{b_n}]: {p['name']}")
+                    # diff_map['missing']['param'].append(f"[{b_n}]: {p['name']}")
+                    if not b_n in diff_map['missing']['param']:
+                        diff_map['missing']['param'][b_n] = []
+                    diff_map['missing']['param'][b_n].append(p)
         else:
             if key == 'nodes':
                 before_params = before[b_n]['parameters']
@@ -71,16 +80,29 @@ for key in keys:
                 after_params = after[b_n]['parameters']
                 after_param_names = [b_p['name'] for b_p in after_params]
                 for b_p_name in before_param_names:
+                    b_p = list(
+                        filter(lambda x: x['name'] == b_p_name,
+                               before_params))[0]
                     if not b_p_name in after_param_names:
-                        diff_map['missing']['param'].append(f"[{b_n}]: {b_p_name}")
+                        # diff_map['missing']['param'].append(f"[{b_n}]: {b_p_name}")
+                        if not b_p_name in diff_map['missing']['param']:
+                            diff_map['missing']['param'][b_n] = []
+                        diff_map['missing']['param'][b_n].append(b_p)
                     else:
-                        b_p = list(filter(lambda x: x['name'] == b_p_name, before_params))[0]
-                        a_p = list(filter(lambda x: x['name'] == b_p_name, after_params))[0]
+                        a_p = list(
+                            filter(lambda x: x['name'] == b_p_name,
+                                   after_params))[0]
                         if b_p['value'] != a_p['value']:
-                            diff_map['change']['param'].append(f"[{b_n}: {b_p_name}] {b_p['value']} -> {a_p['value']}")
+                            # diff_map['change']['param'].append(f"[{b_n}: {b_p_name}] {b_p['value']} -> {a_p['value']}")
+                            if not b_n in diff_map['change']['param']:
+                                diff_map['change']['param'][b_n] = []
+                            diff_map['change']['param'][b_n].append({"name": b_p_name, "before": b_p['value'], "after": a_p['value'], "type": b_p['type']})
                 for a_p_name in after_param_names:
                     if not a_p_name in before_param_names:
-                        diff_map['add']['param'].append(f"[{b_n}]: {a_p_name}")
+                        # diff_map['add']['param'].append(f"[{b_n}]: {a_p_name}")
+                        if not a_p_name in diff_map['add']['param']:
+                            diff_map['add']['param'][b_n] = []
+                        diff_map['add']['param'][b_n].append(b_p)
     for idx_after, a_n in enumerate(after_names):
 
         if not a_n in before_names:
@@ -88,7 +110,10 @@ for key in keys:
             if key == 'nodes':
                 params = after[a_n]['parameters']
                 for p in params:
-                    diff_map['add']['param'].append(f"[{a_n}]: {p['name']}")
+                    # diff_map['add']['param'].append(f"[{a_n}]: {p['name']}")
+                    if not a_n in diff_map['add']['param']:
+                        diff_map['add']['param'][a_n] = []
+                    diff_map['add']['param'][a_n].append(p)
 
 
 with open("./diff_result.json", 'w', encoding='utf-8') as fout:
